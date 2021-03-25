26bb5381ffe34c264170f0e336c8500f11f53d18
Samarth <samarthnasnodkar@gmail.com>
2021-03-25 11:19:24 +0530

    Created better directories

---
+++ src/utils/COLORS!.hpp
@@ -0,0 +1,18 @@
+char GREEN_NO_FLASH[] = "\033[0;32m"; // Non flashing green text
+char GREEN_FLASH[] = "\033[0;32;5m"; // Flashing green text
+char GREEN_UNDER[] = "\033[0;32;4m"; // Underlined Green Text
+char GREEN_NO_UNDER[] = "\033[0;32m"; // Non-Underlined Green Text
+
+char ORANGE_NO_FLASH[] = "\033[1;33m"; // Non flashing orange/yellow text
+char ORANGE_FLASH[] = "\033[1;33;5m"; // Flashing orange/yellow text
+char ORANGE_UNDER[] = "\033[1;33;4m"; // Underlined Orange Text
+char ORANGE_NO_UNDER[] = "\033[1;33m"; // Non-Underlined Orange Text
+
+char RED_NO_FLASH[] = "\033[1;31m"; // Non flashing red text
+char RED_FLASH[] = "\033[1;31;5m"; // Flashing red text
+char RED_UNDER[] = "\033[1;31;4m"; // Underlined Red Text
+char RED_NO_UNDER[] = "\033[1;31m"; // Non-Underlined Red Text
+
+char BG_WHITE[] = "\033[47;30;1m"; //White background
+
+char NC[] = "\033[0m"; // NO COLOR. Resets Text Color
---
+++ src/utils/conio.h
@@ -0,0 +1,46 @@
+#include <termios.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/ioctl.h>
+
+bool kbhit()
+{
+    termios term;
+    tcgetattr(0, &term);
+
+    termios term2 = term;
+    term2.c_lflag &= ~ICANON;
+    tcsetattr(0, TCSANOW, &term2);
+
+    int byteswaiting;
+    ioctl(0, FIONREAD, &byteswaiting);
+
+    tcsetattr(0, TCSANOW, &term);
+
+    return byteswaiting > 0;
+}
+int getch(void)
+{
+    struct termios oldattr, newattr;
+    int ch;
+    tcgetattr(STDIN_FILENO, &oldattr);
+    newattr = oldattr;
+    newattr.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newattr);
+    ch = getchar();
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldattr);
+    return ch;
+}
+
+int getche(void)
+{
+    struct termios oldattr, newattr;
+    int ch;
+    tcgetattr(STDIN_FILENO, &oldattr);
+    newattr = oldattr;
+    newattr.c_lflag &= ~(ICANON);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newattr);
+    ch = getchar();
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldattr);
+    return ch;
+}
